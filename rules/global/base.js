1226c02bd1fcfb2b489ebf90cd64fc4269365894
Victor Godinho <dragonsgamers@hotmail.com.br>
2023-01-02 22:14:49 -0300

    feat: :children_crossing: lint process plugin

--- rules/global/base.js
+++ rules/global/base.js
@@ -2,7 +2,20 @@ const indentSize = 4;
 
 module.exports = {
     rules: {
-        "indent": [ "error", indentSize ], // 4 spaces
+        "indent": [ "error", indentSize, {
+            SwitchCase: 1,
+            VariableDeclarator: 1,
+            outerIIFEBody: 1,
+            MemberExpression: 1,
+            FunctionDeclaration: { parameters: 1, body: 1 },
+            FunctionExpression: { parameters: 1, body: 1 },
+            CallExpression: { arguments: 1 },
+            ArrayExpression: 1,
+            ObjectExpression: 1,
+            ImportDeclaration: 1,
+            flatTernaryExpressions: false,
+            ignoreComments: false,
+        } ], // 4 spaces
         "quotes": [ "error", "double" ], // Aspas duplas
         "capitalized-comments": [ "error" ], // Comentários devem ser iniciados em letras maiúsculas
         "space-before-function-paren": [ "error", {
--- rules/typescript/best-practices.js
+++ rules/typescript/best-practices.js
@@ -10,7 +10,19 @@ module.exports = {
             "error",
             indentSize,
             {
-                "ignoredNodes": [ "TSTypeParameterInstantiation" ],
+                SwitchCase: 1,
+                VariableDeclarator: 1,
+                outerIIFEBody: 1,
+                MemberExpression: 1,
+                FunctionDeclaration: { parameters: 1, body: 1 },
+                FunctionExpression: { parameters: 1, body: 1 },
+                CallExpression: { arguments: 1 },
+                ArrayExpression: 1,
+                ObjectExpression: 1,
+                ImportDeclaration: 1,
+                flatTernaryExpressions: false,
+                ignoreComments: false,
+                ignoredNodes: [ "TSTypeParameterInstantiation" ],
             },
         ], // Força indent com 4 espaços
         "quotes": [ "off" ], // Aspas duplas
